793b3e2155c06ddd4bea32994aa86229530405cf
Kelvin Ndoma <kelvinndomamutua@gmail.com>
2025-11-19 01:55:27 +0300

    cors

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(cors({
     origin: [
         'https://tbb-project-management.vercel.app',
+        'https://www.tbbasco.com',
         'http://127.0.0.1:5173', 
         'http://localhost:5173'
     ],
